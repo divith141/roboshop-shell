b2a355d5f6f96341a33729d819c726fab00cb66d
Kranthi <kranthi.nanu@gmail.com>
2024-07-16 03:53:23 -0700

    Roboshop user script

--- cart.sh
+++ cart.sh
@@ -3,7 +3,7 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 mkdir /app
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
 cd /app
 unzip /tmp/cart.zip
 cd /app
